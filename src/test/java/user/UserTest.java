ac787272ed9de97b78f80310e6b99eaed932fbc2
roman <roman.pshenichnikov555@gmail.com>
2023-02-01 19:56:01 +0300

    Course4_HW2 done

---
+++ src/test/java/user/UserTest.java
@@ -0,0 +1,35 @@
+package user;
+
+import org.example.user.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+    @Test
+    @DisplayName("When login and email to user return correct")
+    public void correctLoginEmailTest() {
+        User user = new User("Vasya", "Vasya123@mail.com");
+        String actualResult = user.getLogin() + " " + user.getEmail();
+        Assertions.assertNotNull(actualResult);
+        Assertions.assertEquals("Vasya Vasya123@mail.com", actualResult);
+    }
+
+    @Test
+    @DisplayName("When email is correct")
+    public void correctEmailTest() {
+        User user = new User("Vasya", "Vasya123@mail.com");
+        String actualResult = user.getLogin() + " " + user.getEmail();
+        Assertions.assertNotNull(actualResult);
+        Assertions.assertEquals("Vasya Vasya123@mail.com", actualResult);
+    }
+
+    @Test
+    @DisplayName("When not equals login and email")
+    public void notEqualsLoginEmailTest() {
+        User user = new User("Vasya", "Vasya123@mail.com");
+        String actualResult = user.getLogin() + " " + user.getEmail();
+        Assertions.assertNotNull(actualResult);
+        Assertions.assertNotEquals("Vasya Vasya123@mail.com", actualResult);
+    }
+}
